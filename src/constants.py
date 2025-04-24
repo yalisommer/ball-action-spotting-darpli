ff086a5c5f26e2b4e5cd2fe7b9aa00928457aa8c
harperaustin <harper_austin@brown.edu>
2025-04-23 22:52:18 -0400

    Update work_dir path to use Colab path

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-work_dir = Path("/workdir")
+work_dir = Path("/content/ball-action-spotting-darpli")
 data_dir = work_dir / "data"
 configs_dir = work_dir / "configs"
 soccernet_dir = data_dir / "soccernet"
